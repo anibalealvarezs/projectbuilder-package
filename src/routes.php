a8a38bc330a0564efb76ef75fa10653852b0b7e2
Anibal Alvarez <anibalealvarezs@github.com>
2020-11-20 03:21:41 +0100

    compatibility con laravel default auth

--- src/routes.php
+++ src/routes.php
@@ -2,14 +2,6 @@
 
 use Anibalealvarezs\Projectbuilder\Controllers\Calculator\PbCalculatorController as Calculator;
 
-Route::namespace('Auth')->group(function () {
-    Route::get('/login','LoginController@show_login_form')->name('login');
-    Route::post('/login','LoginController@process_login')->name('login');
-    Route::get('/register','LoginController@show_signup_form')->name('register');
-    Route::post('/register','LoginController@process_signup');
-    Route::post('/logout','LoginController@logout')->name('logout');
-});
-
 Route::get('add/{a}/{b}', [Calculator::class, 'add'])->where(['a' => '[-0-9]+', 'b' => '[0-9]+']);
 Route::get('substract/{a}/{b}', [Calculator::class, 'substract'])->where(['a' => '[-0-9]+', 'b' => '[0-9]+']);
 
