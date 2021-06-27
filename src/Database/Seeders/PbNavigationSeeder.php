af573ab21d1091cbfbd2f062ddd1bf8ba054d7ad
Aníbal Álvarez <anibalealvarezs@gmail.com>
2021-06-26 22:13:58 -0400

    PbNavigationSeeder hotfix

--- src/Database/Seeders/PbNavigationSeeder.php
+++ src/Database/Seeders/PbNavigationSeeder.php
@@ -3,6 +3,7 @@
 namespace Anibalealvarezs\Projectbuilder\Database\Seeders;
 
 use Anibalealvarezs\Projectbuilder\Models\PbNavigation as Navigation;
+use Anibalealvarezs\Projectbuilder\Models\PbPermission;
 use Illuminate\Database\Seeder;
 
 class PbNavigationSeeder extends Seeder
@@ -15,19 +16,22 @@ class PbNavigationSeeder extends Seeder
     public function run()
     {
         // Parents
-        Navigation::create(['name' => 'Dashboard', 'destiny' => 'dashboard', 'type' => 'route', 'parent' => 0, 'permission' => ['login'], 'nodule' => null]);
-        $usersParent = Navigation::create(['name' => 'Users & Roles', 'destiny' => '#navigation-users-roles', 'type' => 'custom', 'parent' => 0, 'permission' => ['read users'], 'nodule' => null]);
-        $settingsParent = Navigation::create(['name' => 'Settings', 'destiny' => '#navigation-settings', 'type' => 'custom', 'parent' => 0, 'permission' => ['crud super-admin'], 'nodule' => null]);
+        Navigation::create(['name' => 'Dashboard', 'destiny' => 'dashboard', 'type' => 'route', 'parent' => 0, 'permission_id' => ['login'], 'nodule' => null]);
+        $usersParent = Navigation::create(['name' => 'Users & Roles', 'destiny' => '#navigation-users-roles', 'type' => 'custom', 'parent' => 0, 'permission_id' => ['read users'], 'nodule' => null]);
+        $settingsParent = Navigation::create(['name' => 'Settings', 'destiny' => '#navigation-settings', 'type' => 'custom', 'parent' => 0, 'permission_id' => ['crud super-admin'], 'nodule' => null]);
 
         // Children
         if ($usersParent) {
-            Navigation::create(['name' => 'Users', 'destiny' => 'users.index', 'type' => 'route', 'parent' => $usersParent->id, 'permission' => ['read users'], 'module' => 'pbuser']);
-            Navigation::create(['name' => 'Roles', 'destiny' => 'roles.index', 'type' => 'route', 'parent' => $usersParent->id, 'permission' => ['admin roles permissions'], 'module' => 'pbroles']);
-            Navigation::create(['name' => 'Permissions', 'destiny' => 'permissions.index', 'type' => 'route', 'parent' => $usersParent->id, 'permission' => ['admin roles permissions'], 'module' => 'pbpermissions']);
+            $readUsersPermission = PbPermission::where('name', 'read users')->first();
+            Navigation::create(['name' => 'Users', 'destiny' => 'users.index', 'type' => 'route', 'parent' => $usersParent->id, 'permission_id' => $readUsersPermission->id, 'module' => 'pbuser']);
+            $AdminRolesPermission = PbPermission::where('name', 'admin roles permissions')->first();
+            Navigation::create(['name' => 'Roles', 'destiny' => 'roles.index', 'type' => 'route', 'parent' => $usersParent->id, 'permission_id' => $AdminRolesPermission->id, 'module' => 'pbroles']);
+            Navigation::create(['name' => 'Permissions', 'destiny' => 'permissions.index', 'type' => 'route', 'parent' => $usersParent->id, 'permission_id' => $AdminRolesPermission->id, 'module' => 'pbpermissions']);
         }
         if ($settingsParent) {
-            Navigation::create(['name' => 'Config', 'destiny' => 'configs.index', 'type' => 'route', 'parent' => $settingsParent->id, 'permission' => ['crud super-admin'], 'module' => 'pbconfig']);
-            Navigation::create(['name' => 'Navigations', 'destiny' => 'navigations.index', 'type' => 'route', 'parent' => $settingsParent->id, 'permission' => ['crud super-admin'], 'module' => 'pbnavigations']);
+            $crudSuperAdminPermission = PbPermission::where('name', 'crud super-admin')->first();
+            Navigation::create(['name' => 'Config', 'destiny' => 'configs.index', 'type' => 'route', 'parent' => $settingsParent->id, 'permission_id' => $crudSuperAdminPermission->id, 'module' => 'pbconfig']);
+            Navigation::create(['name' => 'Navigations', 'destiny' => 'navigations.index', 'type' => 'route', 'parent' => $settingsParent->id, 'permission_id' => $crudSuperAdminPermission->id, 'module' => 'pbnavigations']);
         }
     }
 }
